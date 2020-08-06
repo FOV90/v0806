2b5e0fdabb47cfdc9bc32e190f6ae4502391631b
Aleksandra <shkolnoe18@gmail.com>
2020-08-06 09:46:42 +0900

    kaizou

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -12,9 +12,30 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        string kao = "(*''▽'')";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+            label1.Left += vx;
+            label1.Top += vy;
+
+            string t=label1.Text;
+            label1.Text=kao;
+            kao=t;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 0;
         }
     }
 }
